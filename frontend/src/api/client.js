3042491c6b5a4ba6a8e7f3267e781c86c3de6e84
MANASA-REDDY04 <kandadimanasareddy12@gmail.com>
2025-11-24 09:37:35 +0530

    added backend hosted url

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+// export const api = axios.create({
+//   baseURL: "http://localhost:4000/api",
+//   withCredentials: true, // important for cookies
+// });
+
 export const api = axios.create({
-  baseURL: "http://localhost:4000/api",
-  withCredentials: true, // important for cookies
+  baseURL: "https://smart-menu-i67q.onrender.com/api",
+  withCredentials: true, 
 });
